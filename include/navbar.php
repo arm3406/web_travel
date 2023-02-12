52638e001e0209bc8a01ec586e4029f5a94f096b
Fajar Permana <fapeput@gmail.com>
2023-02-12 22:07:47 +0700

    Revert "Revert "add footer&galeri""
    This reverts commit 5abe110a8476b2523392b1a37cc7c8bc7d9f54c4.

---
+++ include/navbar.php
@@ -0,0 +1,27 @@
+<!-- navbar -->
+
+<div class="navbar p-0">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary col" style="background-color: #eaeaea;">
+        <div class="container-fluid ">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="d-flex justify-content-end">
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 r-2 ">
+
+                        <li class="nav-item p-2">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item p-2">
+                            <a class="nav-link active" aria-current="page" href="galeri.php">Galeri</a>
+                        </li>
+                        <li class="nav-item p-2">
+                            <a class="nav-link active" aria-current="page" href="profil.php">Profil</a>
+                        </li>
+                </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
